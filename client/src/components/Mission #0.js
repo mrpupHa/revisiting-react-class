9dd10d77dce52330b2350868275a36fe7739827d
mrbuba <wwpptew101@gmail.com>
2026-01-05 16:02:31 +0700

    feat: refactor App component to use new layout with Navbar, ProductHighlight, ProductCategory, and Footer components

---
+++ client/src/components/Mission #0.js
@@ -0,0 +1,8 @@
+// - ขอให้ตอบคำถามต่อไปนี้
+//     - NPM คืออะไร => คือตัวจัดการ package ของ Node.js
+//     - Vite คืออะไร => คือตัวจัดการ build ของ React
+//     - React Folder structure ด้านล่างแต่ละตัวคืออะไร
+//         - package.json => คือตัวจัดการ package ของ React
+//         - node_modules => คือตัวจัดการ package ของ Node.js
+//         - .gitignore => คือตัวจัดการไฟล์ที่ไม่ต้องการ push ขึ้นไปบน remote repository
+//         - App.jsx => คือไฟล์ที่สร้างขึ้นมาเพื่อสร้างไว้เก็บ render ของ component ต่าง ๆ บนหน้าเว็บ react
