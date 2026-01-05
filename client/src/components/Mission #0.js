571210da816fecc2ad6367c99fe9be86f44c00d8
mrbuba <wwpptew101@gmail.com>
2026-01-05 18:03:11 +0700

    fix: update comments in Mission #0.js

--- client/src/components/Mission #0.js
+++ client/src/components/Mission #0.js
@@ -1,8 +1,8 @@
 // - ขอให้ตอบคำถามต่อไปนี้
-//     - NPM คืออะไร => คือตัวจัดการ package ของ Node.js
-//     - Vite คืออะไร => คือตัวจัดการ build ของ React
+//     - NPM คืออะไร => คือตัวจัดการ package ของ javascript, nodejs
+//     - Vite คืออะไร => คือเครื่องมือสร้างและรันโปรเจกต์ เช่น run dev server
 //     - React Folder structure ด้านล่างแต่ละตัวคืออะไร
-//         - package.json => คือตัวจัดการ package ของ React
-//         - node_modules => คือตัวจัดการ package ของ Node.js
+//         - package.json => คือตัวจัดการ package npm ทั้งหมดของโปรเจกต์ที่เราทำใช้ lib อะไรบ้าง คำสั่งอะไร run ยังไงบ้าง เป็นต้น
+//         - node_modules => คือโฟลเดอร์ที่เก็บ library ที่เรา install เข้าไปในโปรเจกต์ทั้งหมด
 //         - .gitignore => คือตัวจัดการไฟล์ที่ไม่ต้องการ push ขึ้นไปบน remote repository
-//         - App.jsx => คือไฟล์ที่สร้างขึ้นมาเพื่อสร้างไว้เก็บ render ของ component ต่าง ๆ บนหน้าเว็บ react
+//         - App.jsx => คือไฟล์ที่สร้างขึ้นมาเพื่อเป็นหน้าหลักในการ render ของ component ต่าง ๆ บนหน้าเว็บ react
